252c4ea96cc28465759ea9416762f24e2ebdb63f
Fabio Albertin <albertin@gmail.com>
2019-07-21 02:13:08 +0100

    Finished first counter app iteration

--- counter/src/index.js
+++ counter/src/index.js
@@ -4,20 +4,47 @@ import hh from 'hyperscript-helpers';
 const { div, button } = hh(h);
 const initModel = 0;
 
-const view = (model) => {
+const update = (msg, model) => {
+  switch(msg) {
+    case 'plus':
+      return model++;
+    case 'minues':
+      return model--;
+    default:
+      return model;
+  }
+};
+
+const view = (dispatch, model) => {
   return div([
     div({ className: 'mv2' }, `Count: ${model}`),
     button({
       className: 'pv1 ph2 mr2',
-      onclick: () => console.log('+ clicked'),
+      onclick: () => dispatch('plus'),
     }, '+'),
     button({
       className: 'pv1 ph2',
-      onclick: () => console.log('- clicked'),
+      onclick: () => dispatch('minus'),
     }, '-'),
   ]);
 }
 
+// Impure composition
+const app = (initModel, update, view, node) => {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+
+  node.appendChild(currentView);
+
+  function dispatch(msg) {
+    model = update(msg, model);
+    const updatedView = view(dispatch, model);
+    node.replaceChild(updatedView, currentView);
+    currentView = updatedView;
+  }
+}
+
 const rootNode = document.getElementById('app');
 
-rootNode.appendChild(view(initModel));
+app(initModel, update, view, rootNode);
+// rootNode.appendChild(view(initModel));
